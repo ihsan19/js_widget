9d9c779107db0dd22735c9259fe2fe067835f177
ihsan <ihsan@41studio.com>
2012-11-02 16:43:36 +0700

    update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -41,12 +41,15 @@ class ContactsController < ApplicationController
   # POST /contacts.json
   def create
     @contact = Contact.new(params[:contact])
+
     respond_to do |format|
       if @contact.save
-        format.js { render json: @contact.to_json }
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.json { render json: @contact, status: :created, location: @contact }
       else
+        format.html { render action: "new" }
         format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end 
+      end
     end
   end
 
